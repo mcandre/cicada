21b5724a3aee81434b2dd6c7eff7135ef7e09981
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2022-06-02 18:25:08 -0500

    bump to v0.0.4

--- cicada.go
+++ cicada.go
@@ -1,7 +1,7 @@
 package cicada
 
 // Version is semver.
-const Version = "0.0.3"
+const Version = "0.0.4"
 
 // LeadMonths provides additional time for engineers
 // to implement version migrations prior to final end of life.
